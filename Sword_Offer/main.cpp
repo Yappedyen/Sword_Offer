35f2a3626b71f40b48fdfc0021df9bc91f068739
PengY <yappedyen@gmail.com>
2021-10-19 22:25:59 +0800

    update main

--- Sword_Offer/main.cpp
+++ Sword_Offer/main.cpp
@@ -12,10 +12,6 @@ int main(){
 	Solution s;
 	int out = s.findRepeatNumber(v);
 	cout << out << endl;
-	int i = 42;
-	int* p1 = &i;
-	*p1 = *p1 * *p1;
-	cout << *p1 << endl;
 	system("pause");
 	return 0;
 }
