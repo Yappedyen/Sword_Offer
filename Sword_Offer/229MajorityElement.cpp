1bfc46804d19ca909f24c7f4d68b32972336ebac
PengY <yappedyen@gmail.com>
2021-10-22 22:10:17 +0800

    update 229

--- Sword_Offer/229MajorityElement.cpp
+++ Sword_Offer/229MajorityElement.cpp
@@ -1,10 +1,12 @@
-#include<iostream>
+﻿#include<iostream>
 #include<vector>
 #include<algorithm>
 #include<unordered_map>
 
 using namespace std;
 
+
+//给定一个大小为 n 的整数数组，找出其中所有出现超过 ⌊ n / 3 ⌋ 次的元素。
 vector<int> majorityElement(vector<int>& nums) {
     int n = nums.size();
     vector<int> ans;
