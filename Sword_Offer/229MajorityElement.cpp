941308745b0a32608eaf0ad2513020aaa119a70e
PengY <yappedyen@gmail.com>
2021-10-22 22:08:42 +0800

    update 229

---
+++ Sword_Offer/229MajorityElement.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<unordered_map>
+
+using namespace std;
+
+vector<int> majorityElement(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> ans;
+    unordered_map<int, int> cnt;
+
+    for (auto& v : nums) {
+        cnt[v]++;
+    }
+    for (auto& v : cnt) {
+        if (v.second > n / 3) {
+            ans.push_back(v.first);
+        }
+    }
+
+    return ans;
+}
