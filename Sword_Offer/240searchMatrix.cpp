3d83a52bd8c5af5d25e1719f64fd0260d0169be5
PengY <yappedyen@gmail.com>
2021-10-26 21:54:29 +0800

    update 496

--- Sword_Offer/240searchMatrix.cpp
+++ Sword_Offer/240searchMatrix.cpp
@@ -1,7 +1,11 @@
-#include<iostream>
+﻿#include<iostream>
 #include<vector>
 using namespace std;
 
+//编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+//
+//每行的元素从左到右升序排列。
+//每列的元素从上到下升序排列。
 
 bool searchMatrix(vector<vector<int>>& matrix, int target) {
     for (const auto& row : matrix) {
---
+++ Sword_Offer/496nextGreaterElement.cpp
@@ -0,0 +1,41 @@
+﻿#include"functions.h"
+#include<unordered_map>
+#include<stack>
+//给你两个 没有重复元素 的数组 nums1 和 nums2 ，其中nums1 是 nums2 的子集。
+//请你找出 nums1 中每个元素在 nums2 中的下一个比其大的值。
+//nums1 中数字 x 的下一个更大元素是指 x 在 nums2 中对应位置的右边的第一个比 x 大的元素。如果不存在，对应位置输出 - 1 。
+
+vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+    int n = nums1.size();
+    vector<int>v;
+    for (int i = 0; i < n; i++) {
+        auto it = find(nums2.begin(), nums2.end(), nums1[i]);
+        for (auto j = it; j != nums2.end(); j++) {
+            if (*j > nums1[i]) {
+                v.push_back(*j);
+                break;
+            }
+            else if (j == --nums2.end()) {
+                v.push_back(-1);
+            }
+        }
+    }
+    return v;
+
+    unordered_map<int, int> hashmap;
+    stack<int> st;
+    for (int i = nums2.size() - 1; i >= 0; --i) {
+        int num = nums2[i];
+        while (!st.empty() && num >= st.top()) {
+            st.pop();
+        }
+        hashmap[num] = st.empty() ? -1 : st.top();
+        st.push(num);
+    }
+    vector<int> res(nums1.size());
+    for (int i = 0; i < nums1.size(); ++i) {
+        res[i] = hashmap[nums1[i]];
+    }
+    return res;
+
+}
