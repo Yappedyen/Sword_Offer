33891e3ab2deb382bb767da4ac71b4564a081b57
PengY <yappedyen@gmail.com>
2021-10-29 20:12:10 +0800

    update 335

--- Sword_Offer/869reorderedPowerOf2.cpp
+++ Sword_Offer/869reorderedPowerOf2.cpp
@@ -1,4 +1,5 @@
 ﻿#include"functions.h"
+#include<unordered_set>
 //给定正整数 N ，我们按任何顺序（包括原始顺序）将数字重新排序，注意其前导数字不能为零。
 //
 //如果我们可以通过上述方式得到 2 的幂，返回 true；否则，返回 false。
@@ -33,3 +34,30 @@ bool reorderedPowerOf2(int n) {
     vis.resize(nums.length());
     return backtrack(nums, 0, 0);
 }
+
+
+//方法二：预处理 + 哈希表
+string countDigits(int n) {
+    string cnt(10, 0);
+    while (n) {
+        ++cnt[n % 10];
+        n /= 10;
+    }
+    return cnt;
+}
+
+unordered_set<string> powerOf2Digits;
+
+int init = []() {
+    for (int n = 1; n <= 1e9; n <<= 1) {
+        powerOf2Digits.insert(countDigits(n));
+    }
+    return 0;
+}();
+
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        return powerOf2Digits.count(countDigits(n));
+    }
+};
